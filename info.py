0055eb064eb1323764960e0cfe4b5e9ddeb5fe99
ranju999 <ranjuvishwakarma.50@gmail.com>
2024-12-30 19:57:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45c
 API_ID = int(getenv("API_ID", "12380656"))
 API_HASH = str(getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", "7803613609:AAHI4VSou6Qs3Du38HSKbfG9_lqhwgyr1iY")
-FORCE_SUB = os.environ.get("FORCE_SUB", "AV_BOTz_UPDATE") 
+FORCE_SUB = os.getenv("FORCE_SUB", "AV_BOTz_UPDATE") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.uvah8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
